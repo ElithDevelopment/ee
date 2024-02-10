e370a3cd5407a0c4a98b62dc06d67f8d680a21c3
Leander Lombardi <leanderlombardi@gmail.com>
2024-02-10 21:58:46 +0100

    Second commit

--- lspack_install_script.sh
+++ lspack_install_script.sh
@@ -4,16 +4,15 @@ if [ -e "/usr/bin/ee" ]; then
   echo "Error: Package 'ee' already exists."
   echo "Installing to '/usr/bin/_ee...'"
   echo "Program will be executable using '_ee'."
-  sudo gcc -o "/usr/bin/_ee" ./src/main.c ./src/encrypt.c ./src/decrypt.c"
-  if [ 0 -ne 0 ]; then
+  sudo gcc -o "/usr/bin/_ee" ./src/main.c ./src/encrypt.c ./src/decrypt.c
+  if [ $? -ne 0 ]; then
     echo "Error installing to '/usr/bin/_ee'."
     exit 1
   fi
 else
   sudo gcc -o "/usr/bin/ee" ./src/main.c ./src/encrypt.c ./src/decrypt.c
-  if [ 0 -ne 0 ]; then
+  if [ $? -ne 0 ]; then
     echo "Error installing to '/usr/bin/ee'."
     exit 1
   fi
 fi
-
